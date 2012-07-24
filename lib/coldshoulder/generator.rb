6904771a26eaa3eef042b68428adff39868b9b7f
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2012-07-24 12:48:05 -0500

    Bit of refactoring.

--- lib/coldshoulder/generator.rb
+++ lib/coldshoulder/generator.rb
@@ -4,8 +4,8 @@ module Coldshoulder
 
     attr_reader :target_language
 
-    def initialize(language = nil)
-      @target_language = language
+    def initialize(args = nil)
+      @target_language = args
     end
 
     def request_url(url)
@@ -13,16 +13,22 @@ module Coldshoulder
     end
 
     def generate
+      build_ignore_file
+    end
+
+    protected 
+
+    def build_ignore_file
       puts 'Generating gitignore file...'
       r = request_url("https://raw.github.com/github/gitignore/master/#{@target_language}.gitignore")
       if r.response_code == 200
         File.open('.gitignore', 'w') do |f|
           f.write("#\n# Generating using coldshoulder - github.com/bryanmikaelian/coldshoulder\n#\n\n#{r.body_str}")
         end
+        puts "Ignore file generated for language #{@target_language}"
       else 
         puts "The gitignore file could not be generated. Ignore file for the language #{@target_language} not found"
       end
-      puts "Ignore file generated for language #{@target_language}"
     end
 
   end
