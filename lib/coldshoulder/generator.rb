04766126458398e1cc7ae6e99990ae9644f2fa04
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2012-07-24 09:29:40 -0500

    Added a generate method.

--- lib/coldshoulder/generator.rb
+++ lib/coldshoulder/generator.rb
@@ -2,8 +2,27 @@ module Coldshoulder
 
   class Generator
 
+    attr_reader :target_language
+
+    def initialize(language = nil)
+      @target_language = language
+    end
+
     def request_url(url)
       Curl::Easy.perform(url)
+    end
+
+    def generate
+      puts 'Generating gitignore file...'
+      r = request_url("https://raw.github.com/github/gitignore/master/#{@target_language}}.gitignore")
+      if r.response_code == 200
+        File.open('.gitignore', 'w') do |f|
+          f.write("#\n# Generating using coldshoulder - github.com/bryanmikaelian/coldshoulder\n#\n#{r.body_str}"
+        end
+      else 
+        puts "The gitignore file could not be generated. Ignore file for the language #{@target_language} not found"
+      end
+      puts "Ignore file generated for language #{@target_language}"
     end
 
   end
--- spec/generator_spec.rb
+++ spec/generator_spec.rb
@@ -1,7 +1,18 @@
 require 'spec_helper'
 
 describe Coldshoulder::Generator do
+
   it 'has a way to get a URL' do
     Coldshoulder::Generator.new.request_url('http://www.google.com').response_code.should == 200
   end
+
+  it 'sets the target language' do
+    c = Coldshoulder::Generator.new('Ruby')
+    c.target_language.should == 'Ruby'
+  end
+
+  it 'generates a gitignore file' do
+    Coldshoulder::Generator.new('Ruby').should respond_to(:generate)
+  end
+
 end
