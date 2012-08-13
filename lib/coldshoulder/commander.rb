3747ba7bb2dc0256f272b78c9dd9103c356d7906
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2012-08-13 12:04:48 -0500

    Logging the args

--- lib/coldshoulder/commander.rb
+++ lib/coldshoulder/commander.rb
@@ -5,6 +5,7 @@ module Coldshoulder
     attr_accessor :command, :language
 
     def initialize(*args)
+      puts args.class
       self.command = args.shift.to_s
       self.language = args.shift
       generate!
@@ -17,8 +18,7 @@ module Coldshoulder
     protected
 
     def process
-      puts self.command.class
-      if self.command === "generate"
+      if self.command == "generate"
         puts 'Calling build in Coldshoulder::Generator'
         Coldshoulder::Generator.new.build(self.language)
       end
