ebeafda10b6774fdb3515b102205f0b6ab76e642
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2012-08-13 13:25:46 -0500

    Now using a hash instead of array

--- spec/commander_spec.rb
+++ spec/commander_spec.rb
@@ -3,18 +3,18 @@ require 'spec_helper'
 describe Coldshoulder::Commander do
 
   it 'takes in a command' do
-    Coldshoulder::Commander.new("generate", "Python").command.should == "generate"
+    Coldshoulder::Commander.new({:command => "generate",:language => "Python"}).command.should == "generate"
   end
 
   it 'takes in a language' do
-    Coldshoulder::Commander.new("generate", "Ruby").language.should == "Ruby"
+    Coldshoulder::Commander.new({:command => "generate",:language => "Ruby"}).language.should == "Ruby"
   end
 
   it 'drives the generation of a gitignore file' do
     file = mock('file')
     File.should_receive(:open).with(".gitignore", "w").and_yield(file)
     file.should_receive(:write)
-    Coldshoulder::Commander.new("generate",'Ruby')
+    Coldshoulder::Commander.new({:command => "generate", :language => "Ruby"}).generate!
   end
   
 end
