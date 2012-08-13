a7d322013ffe24681d7d1f20475dfb4215b093ee
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2012-08-13 12:08:13 -0500

    refactoring'

--- lib/coldshoulder/commander.rb
+++ lib/coldshoulder/commander.rb
@@ -5,21 +5,15 @@ module Coldshoulder
     attr_accessor :command, :language
 
     def initialize(*args)
-      self.command = args.shift.to_s
+      self.command = args.shift
       self.language = args.shift
       generate!
     end
 
     def generate!
-      process
+      Coldshoulder::Generator.new.build(@language)
     end
 
-    protected
-
-    def process
-      puts 'Calling build in Coldshoulder::Generator'
-      Coldshoulder::Generator.new.build(self.language)
-    end
   end
   
 end
