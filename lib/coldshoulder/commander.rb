73add450c2c4c41c8570192d06414029491f92e9
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2012-08-13 11:49:41 -0500

    Logging

--- lib/coldshoulder/commander.rb
+++ lib/coldshoulder/commander.rb
@@ -20,6 +20,7 @@ module Coldshoulder
     protected
 
     def process
+      puts self.command.class
       if self.command === "generate"
         puts 'Calling build in Coldshoulder::Generator'
         Coldshoulder::Generator.new.build(self.language)
