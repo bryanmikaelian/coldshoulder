95c934911e15f035b9ffb35e8655832a9fad546e
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2012-08-14 14:19:53 -0500

    Now outputting the version of coldshoulder in the generated gitignore file.

--- lib/coldshoulder/generator.rb
+++ lib/coldshoulder/generator.rb
@@ -12,7 +12,7 @@ module Coldshoulder
       r = request_url("https://raw.github.com/github/gitignore/master/#{language}.gitignore")
       if r.response_code == 200 
         File.open('.gitignore', 'w') do |f|
-          f.write("#\n# Generated using coldshoulder - github.com/bryanmikaelian/coldshoulder\n#\n\n#{r.body_str}")
+          f.write("#\n# Generated using coldshoulder v#{Coldshoulder::VERSION} - github.com/bryanmikaelian/coldshoulder\n#\n\n#{r.body_str}")
         end
         puts "Ignore file generated for language #{language}"
       else 
--- lib/coldshoulder/version.rb
+++ lib/coldshoulder/version.rb
@@ -1,3 +1,3 @@
 module Coldshoulder
-  VERSION = "1.1.1"
+  VERSION = "1.1.2"
 end
